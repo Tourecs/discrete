e9060daccd259a787573d4fcef07fe476ea495ce
Tourecs <sverlov2006@mail.ru>
2025-06-04 20:29:05 +0600

    Add files via upload

---
+++ №1 Дискра (10.02) Уник посещ/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+    class Program
+    {
+        static void Main()
+        {
+            int[,] graph ={
+            {1,1,1,0,1,1},
+            {0,1,0,1,0,0},
+            {1,0,1,0,0,1},
+            {1,0,1,0,0,0},
+            {0,0,1,1,1,0},
+            {0,0,1,0,1,0},
+            };
+
+            List<int> visit = new List<int>();
+            int result = 0;
+
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                if (!visit.Contains(i))
+                {
+                    DFS(graph, i, visit);
+                    result++;
+                }
+            }
+
+            Console.WriteLine("Кол-во уникальных посещенных узлов равно " + result);
+        }
+
+        static void DFS(int[,] graph, int node, List<int> visit)
+        {
+            visit.Add(node);
+
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                if (graph[node, i] == 1 && !visit.Contains(i))
+                {
+                    DFS(graph, i, visit);
+                }
+            }
+        }
+    }
---
+++ №2.2 Дискра (13.02) Прима/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+    class Graph
+    {
+        private int V;
+        private List<(int destination, int weight)>[] adjacencyList;
+
+        public Graph(int vertices)
+        {
+            V = vertices;
+            adjacencyList = new List<(int, int)>[V];
+            for (int i = 0; i < V; i++)
+            {
+                adjacencyList[i] = new List<(int, int)>();
+            }
+        }
+
+        public void AddEdge(int source, int destination, int weight)
+        {
+            adjacencyList[source].Add((destination, weight));
+            adjacencyList[destination].Add((source, weight)); // Для неориентированного графа
+        }
+
+        public void Prim()
+        {
+            bool[] inMST = new bool[V];
+            int[] minEdge = new int[V];
+            int[] parent = new int[V];
+
+            Array.Fill(minEdge, int.MaxValue);
+            minEdge[0] = 0; // Начинаем с первого узла
+            parent[0] = -1;
+
+            for (int count = 0; count < V - 1; count++)
+            {
+                int u = SelectMinVertex(inMST, minEdge);
+                inMST[u] = true;
+
+                foreach (var (v, weight) in adjacencyList[u])
+                {
+                    if (!inMST[v] && weight < minEdge[v])
+                    {
+                        minEdge[v] = weight;
+                        parent[v] = u;
+                    }
+                }
+            }
+
+            PrintMST(parent, minEdge);
+        }
+
+        private int SelectMinVertex(bool[] inMST, int[] minEdge)
+        {
+            int minVertex = -1;
+            for (int i = 0; i < V; i++)
+            {
+                if (!inMST[i] && (minVertex == -1 || minEdge[i] < minEdge[minVertex]))
+                {
+                    minVertex = i;
+                }
+            }
+            return minVertex;
+        }
+
+        private void PrintMST(int[] parent, int[] minEdge)
+        {
+        int minimumCost = 0;
+            for (int i = 1; i < V; i++)
+            {
+                Console.WriteLine($"({parent[i]}, {i}), вес: {minEdge[i]}");
+                minimumCost += minEdge[i];
+            }
+            Console.WriteLine("Минимальное остовное дерево:" + minimumCost);
+    }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph g = new Graph(4);
+            g.AddEdge(0, 1, 7);
+            g.AddEdge(0, 2, 3);
+            g.AddEdge(0, 3, 8);
+            g.AddEdge(1, 2, 5);
+            g.AddEdge(1, 3, 12);
+            g.AddEdge(2, 3, 2);
+
+            g.Prim();
+        }
+    }
